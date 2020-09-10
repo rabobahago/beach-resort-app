b9b32988a1e98814eeb939fc0c486dcbfd0b6a95
rabobahago <rabobahago@gmail.com>
2020-09-10 13:11:11 +0100

    NavBar component features added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Error from './pages/Error'
 import SingleRoom from './pages/SingleRoom'
 import Rooms from './pages/Rooms'
 import { Route, Switch } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
-        <Route path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
       </Switch>
     </>
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+import { FaAlignRigt } from 'react-icons/fa'
+class NavBar extends React.Component {
+  render() {
+    return <div>Hello from NavBar component</div>
+  }
+}
+export default NavBar
