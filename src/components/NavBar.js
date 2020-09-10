65225e408dfaad85cee328d0d0c1496d62dc4a1e
rabobahago <rabobahago@gmail.com>
2020-09-10 13:40:09 +0100

    finished navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,50 @@
 import React from 'react'
 import logo from '../images/logo.svg'
-import { FaAlignRigt } from 'react-icons/fa'
+import { FaAlignRight } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+    }
+  }
+  handleIsOpen = () => {
+    this.setState((prevState) => {
+      return {
+        isOpen: !prevState.isOpen,
+      }
+    })
+  }
   render() {
-    return <div>Hello from NavBar component</div>
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-Header">
+            <Link to="/">
+              <img src={logo} alt="Beach Resort" />
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleIsOpen}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? 'nav-links : show-nav' : 'nav-links'}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 export default NavBar
