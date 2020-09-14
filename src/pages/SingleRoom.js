4dddc816c269ce9e278398f6c387ef538ff9055a
rabobahago <rabobahago@gmail.com>
2020-09-14 08:40:49 +0100

    finished single room component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -32,15 +32,17 @@ class SingleRoom extends React.Component {
       description,
       capacity,
       images,
-      prices,
+      price,
       size,
       extras,
       breakfast,
       pets,
     } = room
+
+    const [mainImg, ...defaultImg] = images
     return (
       <>
-        <StyledHero img={images[0] || this.state.defaultBcg}>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
           <Banner title={`${name} room`}>
             <Link to="/rooms" className="btn-primary">
               Back to rooms
@@ -49,10 +51,35 @@ class SingleRoom extends React.Component {
         </StyledHero>
         <section className="single-room">
           <div className="single-room-images">
-            {images.map((item, index) => {
+            {defaultImg.map((item, index) => {
               return <img key={index} src={item} alt={name} />
             })}
           </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>Info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: {size} SQFT</h6>
+              <h6>
+                Max Capacity :{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? 'pet allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section room-extras>
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>-{item}</li>
+            })}
+          </ul>
         </section>
       </>
     )
